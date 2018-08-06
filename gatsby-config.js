b9a806af4347b8b9c7922c37289f3c9f7dadd764
cupcake <jbw@users.noreply.github.com>
2018-08-06 17:27:46 +0100

    remove token from repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,10 +30,9 @@ module.exports = {
         {
             resolve: 'gatsby-source-contentful',
             options: {
-                spaceId: '57zqw4qqv6jg',
-                accessToken: '4f0a37d0f56ae49f18c4e1dc81f1dcc8f6b81a49f07a5e21bacf20ed5265033b',
+                spaceId: process.env.CONTENTFUL_SPACE,
+                accessToken: process.env.CONTENTFUL_TOKEN,
             },
         },
-     
     ],
 };
