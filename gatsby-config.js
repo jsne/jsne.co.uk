21056b3e0a79b48a1bb624576749471210f1099a
cupcake <jbw@users.noreply.github.com>
2018-08-06 17:38:45 +0100

    revert autoformat

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,43 @@
 
 require('dotenv').config();
 
-export const siteMetadata = {
-    title: 'JSNE Website',
-};
+module.exports = {
 
-export const plugins = [
-    'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-emotion',
-    {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            path: `${__dirname}/src/pages`,
-            name: 'pages',
-        },
+    siteMetadata: {
+        title: 'JSNE Website',
     },
-    {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            path: `${__dirname}/src/img`,
-            name: 'images',
+
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        // 'gatsby-plugin-emotion',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
         },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-        resolve: 'gatsby-transformer-remark',
-        options: {
-            plugins: [],
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/img`,
+                name: 'images',
+            },
         },
-    },
-    {
-        resolve: 'gatsby-source-contentful',
-        options: {
-            spaceId: process.env.CONTENTFUL_SPACE,
-            accessToken: process.env.CONTENTFUL_TOKEN,
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [],
+            },
         },
-    },
-];
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE,
+                accessToken: process.env.CONTENTFUL_TOKEN,
+            },
+        },
+    ]
+};
