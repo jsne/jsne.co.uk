9ac5601762e02f4b45fe9ace21f7f0911a5736cb
cupcake <jbw@users.noreply.github.com>
2018-08-06 17:34:36 +0100

    use dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,40 @@
-module.exports = {
-    siteMetadata: {
-        title: 'JSNE Website',
-    },
-    plugins: [
-        'gatsby-plugin-react-helmet',
-        // 'gatsby-plugin-emotion',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                path: `${__dirname}/src/pages`,
-                name: 'pages',
-            },
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                path: `${__dirname}/src/img`,
-                name: 'images',
-            },
-        },
-        'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp',
-        {
-            resolve: 'gatsby-transformer-remark',
-            options: {
-                plugins: [],
-            },
-        },
-        {
-            resolve: 'gatsby-source-contentful',
-            options: {
-                spaceId: process.env.CONTENTFUL_SPACE,
-                accessToken: process.env.CONTENTFUL_TOKEN,
-            },
-        },
-    ],
+
+require('dotenv').config();
+
+export const siteMetadata = {
+    title: 'JSNE Website',
 };
+
+export const plugins = [
+    'gatsby-plugin-react-helmet',
+    // 'gatsby-plugin-emotion',
+    {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            path: `${__dirname}/src/pages`,
+            name: 'pages',
+        },
+    },
+    {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            path: `${__dirname}/src/img`,
+            name: 'images',
+        },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+        resolve: 'gatsby-transformer-remark',
+        options: {
+            plugins: [],
+        },
+    },
+    {
+        resolve: 'gatsby-source-contentful',
+        options: {
+            spaceId: process.env.CONTENTFUL_SPACE,
+            accessToken: process.env.CONTENTFUL_TOKEN,
+        },
+    },
+];
