01600111cf2f976d1ae92b128a568c2dfc868db0
nerdyman <averynerdyman@gmail.com>
2018-10-23 17:59:55 +0100

    Allow JSX import with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,9 +37,10 @@ module.exports = {
         }],
         // react
         'react/forbid-prop-types': 0,
-        'react/jsx-filename-extension': 0,
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
         'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
+        'react/jsx-one-expression-per-line': 0,
         'react/prefer-stateless-function': 0,
         'react/require-default-props': 0,
     },
