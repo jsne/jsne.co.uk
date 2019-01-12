e14c13e7237a7960485d3f874f23b3f9e6316400
jbw <jbw@users.noreply.github.com>
2019-01-12 11:19:24 +0000

    add conduct page with content only

--- src/pages/conduct.js
+++ src/pages/conduct.js
@@ -1,7 +1,42 @@
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
 
+import Base from 'Components/views/Base';
 
-const ContentPage = () => [<div>Hello</div>];
+const ContentPage = ({
+    data: {
+        contentfulPage
+    },
+}) => {
 
+    const { title, introduction, body } = contentfulPage;
+
+    return (
+        <Base>
+            <div>{title}</div>
+            <div>{introduction.introduction}</div>
+            <div>{body.body}</div>
+        </Base>
+    );
+};
+
+ContentPage.propTypes = {
+    data: PropTypes.object,
+};
+
+export const pageQuery = graphql`
+    query conductQuery {
+        contentfulPage(slug: {eq: "/conduct"}) {
+            title
+            body {
+                body
+            }
+            introduction {
+                introduction
+            }
+        }
+    }
+`;
 
 export default ContentPage;
