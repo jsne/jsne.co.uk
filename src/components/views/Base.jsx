3a448ac11d95c3772596219b68898e35256d5e1f
nerdyman <averynerdyman@gmail.com>
2019-02-06 00:07:35 +0000

    Resolves #13 - Use ThemeProvider

--- src/components/views/Base.jsx
+++ src/components/views/Base.jsx
@@ -2,9 +2,12 @@
  * @module Base
  */
 
+import { ThemeProvider } from 'emotion-theming';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import theme from 'Styles/theme';
 
 import 'Static/styles.css';
 
@@ -20,31 +23,33 @@ const Base = ({
     title = 'JavaScript North East | JSNE',
     ...props
 }) => (
-    <main {...props}>
-        <Helmet
-            title={title}
-            meta={[
-                { name: 'description', content: description },
-                { name: 'keywords', content: 'JavaScript, meet-up, JSNE' },
-                { name: 'msapplication-TileColor', content: 'red' },
-                { name: 'theme-color', content: 'blue' },
-                ...metas,
-            ]}
-            link={[
-                /* eslint-disable object-curly-newline */
-                { href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css', rel: 'stylesheet prefetch preload', as: 'style' },
-                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-                { rel: 'manifest', href: '/site.webmanifest' },
-                { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#6030c6' },
-                ...links,
-                /* eslint-enable object-curly-newline */
-            ]}
-            script={scripts}
-        />
-        {children}
-    </main>
+    <ThemeProvider theme={theme}>
+        <main {...props}>
+            <Helmet
+                title={title}
+                meta={[
+                    { name: 'description', content: description },
+                    { name: 'keywords', content: 'JavaScript, meet-up, JSNE' },
+                    { name: 'msapplication-TileColor', content: theme.color.primaryBase },
+                    { name: 'theme-color', content: theme.color.primaryBase },
+                    ...metas,
+                ]}
+                link={[
+                    /* eslint-disable object-curly-newline */
+                    { href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css', rel: 'stylesheet prefetch preload', as: 'style' },
+                    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                    { rel: 'manifest', href: '/site.webmanifest' },
+                    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#6030c6' },
+                    ...links,
+                    /* eslint-enable object-curly-newline */
+                ]}
+                script={scripts}
+            />
+            {children}
+        </main>
+    </ThemeProvider>
 );
 
 Base.propTypes = {
