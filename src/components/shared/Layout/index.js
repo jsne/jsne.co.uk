040f9ae5e5a9f5f2fa54357ce0f2c40aee0e0771
nerdyman <averynerdyman@gmail.com>
2019-07-14 03:25:34 +0100

    Remove old components

--- src/components/shared/Layout/index.js
+++
@@ -1,3 +0,0 @@
-import LayoutRoot from './LayoutRoot';
-
-export { LayoutRoot };
--- src/components/shared/LoadingScreen/index.js
+++
@@ -1,5 +0,0 @@
-import LoadingScreen from './LoadingScreen';
-import DomLoadingScreen from './DomLoadingScreen';
-
-export { DomLoadingScreen };
-export default LoadingScreen;
