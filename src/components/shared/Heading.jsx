be05f3671f84c815f608af4a475ef6a61dfac74c
nerdyman <averynerdyman@gmail.com>
2019-07-14 03:26:08 +0100

    Add heading css util

--- src/components/shared/Heading.jsx
+++ src/components/shared/Heading.jsx
@@ -1,67 +1,8 @@
-/**
- * @module Heading
- * @desc Standard heading components
- */
+import { css } from '@emotion/core';
 
-import PropTypes from 'prop-types';
-import React from 'react';
-import styled from 'react-emotion';
-
-const AnchorHeadingRoot = styled('div')`
-    display: flex;
-    color: ${props => props.theme.color.uiPageForegroundBase};
-`;
-
-const AnchorHeadingAnchor = styled('a')`
-    ${props => props.theme.transition.call('color')}
-    margin-right: ${props => props.theme.spacing.half};
-    color: ${props => props.theme.color.uiPageAnchorBase};
-
-    &:hover,
-    &:focus {
-        color: ${props => props.theme.color.uiPageAnchorActive};
-    }
-`;
-
-export const BaseAnchorHeading = ({
-    anchor,
-    children,
-    ...props
-}) => {
-    // Fallback ID to safe and friendly(ish) string
-    const id = anchor || String(children).replace(/[^\w\s]/gi, '').replace(/ /g, '-').toLowerCase();
-
-    return (
-        <AnchorHeadingRoot id={id} {...props}>
-            <AnchorHeadingAnchor href={`#${id}`}>#</AnchorHeadingAnchor>
-            {children}
-        </AnchorHeadingRoot>
-    );
-};
-
-BaseAnchorHeading.propTypes = {
-    anchor: PropTypes.string,
-    children: PropTypes.node,
-};
-
-// @NOTE Wrapping component since emotion 9 doesn't support `as` prop
-export const H1 = styled(({ className, ...props }) => (
-    <h1 className={className}><BaseAnchorHeading as="h1" {...props} /></h1>
-))`
-    font-size: ${props => props.theme.fontSize.larger};
-    margin-bottom: ${props => props.theme.spacing.half};
-`;
-
-export const H2 = styled(({ className, ...props }) => (
-    <h2 className={className}><BaseAnchorHeading as="h1" {...props} /></h2>
-))`
-    font-size: ${props => props.theme.fontSize.large};
-    margin-bottom: ${props => props.theme.spacing.quarter};
-`;
-
-export const H3 = styled(({ className, ...props }) => (
-    <h3 className={className}><BaseAnchorHeading as="h1" {...props} /></h3>
-))`
-    font-size: ${props => props.theme.fontSize.medium};
-    margin-bottom: ${props => props.theme.spacing.quarter};
+export const headingCssFont = ({ primary = true, ...props }) => css`
+    font-family: ${primary
+        ? props.theme.fontFamily.title0
+        : props.theme.fontFamily.title1};
+    font-weight: normal;
 `;
