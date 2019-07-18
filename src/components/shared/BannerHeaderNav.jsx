3625e97626191ea02f3dd5a96d66a218fe09a038
nerdyman <averynerdyman@gmail.com>
2019-07-18 22:54:37 +0100

    Add super basic burger menu functionality

--- src/components/shared/BannerHeaderNav.jsx
+++ src/components/shared/BannerHeaderNav.jsx
@@ -1,23 +1,41 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 import { usePageQueryAllNavFormatted } from 'Graphql/page';
 
 import { listCleanCss } from 'Styles/utils';
 
+import IconBars from 'Assets/images/icon-bars.svg';
+
 import { Button } from 'Components/shared/Button';
+import { VisuallyHidden } from 'Components/shared/VisuallyHidden';
 
 const BannerHeaderNavToggle = styled(props => (
     <Button as="button" {...props} />
 ))`
+    padding: ${props =>
+        `${props.theme.spacing.eighth} ${props.theme.spacing.quarter}`};
     color: ${props => props.theme.color.brand1Base};
 `;
 
-const BannerHeaderNavRoot = styled.nav``;
+const BannerHeaderNavToggleIcon = styled(IconBars)`
+    width: 1.75rem;
+    fill: currentColor;
+`;
+
+const BannerHeaderNavRoot = styled.nav`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
 
 const BannerHeaderNavListRoot = styled.ul`
     ${listCleanCss}
+    display: ${props => (props.isExpanded ? 'block' : 'none')};
+    position: absolute;
+    top: 100%;
+    right: 0;
 `;
 
 const BannerHeaderNavListItemRoot = styled.li``;
@@ -43,8 +61,8 @@ BannerHeaderNavListItem.propTypes = {
     title: PropTypes.string.isRequired,
 };
 
-const BannerHeaderNavList = ({ navItems, ...props }) => (
-    <BannerHeaderNavListRoot {...props}>
+const BannerHeaderNavList = ({ isExpanded, navItems, ...props }) => (
+    <BannerHeaderNavListRoot isExpanded={isExpanded} {...props}>
         {navItems.map(item => (
             <BannerHeaderNavListItem key={item.slug} {...item} />
         ))}
@@ -52,17 +70,28 @@ const BannerHeaderNavList = ({ navItems, ...props }) => (
 );
 
 BannerHeaderNavList.propTypes = {
+    isExpanded: PropTypes.bool,
     navItems: PropTypes.arrayOf(PropTypes.object),
 };
 
 export const BannerHeaderNav = ({ toggleButtonLabel = 'Toggle', ...props }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
     const navItems = usePageQueryAllNavFormatted();
+
+    const handleToggleClick = (/* ev*/) => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
         <BannerHeaderNavRoot role="navigation" {...props}>
-            <BannerHeaderNavToggle href="#">
-                {toggleButtonLabel}
+            <BannerHeaderNavToggle
+                onClick={handleToggleClick}
+                aria-expanded={isExpanded}
+            >
+                <BannerHeaderNavToggleIcon role="presentation" />
+                <VisuallyHidden>{toggleButtonLabel}</VisuallyHidden>
             </BannerHeaderNavToggle>
-            <BannerHeaderNavList navItems={navItems} />
+            <BannerHeaderNavList isExpanded={isExpanded} navItems={navItems} />
         </BannerHeaderNavRoot>
     );
 };
